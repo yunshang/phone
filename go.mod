f0566a22bf9421aea3e8d7714079c8b3780f6298
alshin <alshin@126.com>
2021-03-23 23:51:56 +0800

    Update

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/yunshang/phone
+
+go 1.14
+
+require (
+	github.com/golobby/config v1.2.0
+	github.com/golobby/config/v2 v2.1.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ phone.go
@@ -0,0 +1,52 @@
+package phoner
+
+import "regexp"
+
+const COMMON_EXTENSIONS = `/[ ]*(ext|ex|x|xt|#|:)+[^0-9]*\(*([-0-9]{1,})\)*#?$/i`
+const COMMON_EXTRAS = `/(\(0\)|[^0-9+]|^\+?00?)/`
+
+var COMMON_EXTRAS_REPLACEMENTS = map[string]string{
+	"00":  "+",
+	"+00": "+",
+	"+0":  "+",
+}
+
+type Phone struct {
+	DefaultCountryCode string
+	DefaultAreaCode    string
+	NamedFormats       string
+	N1Length           string
+}
+
+func (p Phone) Valid(s string) {
+
+}
+
+func parse(s string) bool {
+	if s == "" {
+		return false
+	}
+	sub, extension := extractExtension(s)
+	sub = normalize(sub)
+	return true
+}
+
+func extractExtension(s string) (string, string) {
+	re := regexp.MustCompile(COMMON_EXTENSIONS)
+	subbed := re.FindString(s)
+	if subbed == "" {
+		return s, ""
+	} else {
+		return subbed, ""
+	}
+}
+
+func normalize(stringWithNumber string) string {
+	re := regexp.MustCompile(COMMON_EXTRAS)
+	match := re.FindAllString(stringWithNumber, -1)
+	var s string
+	for _, m := range match {
+		s = COMMON_EXTRAS_REPLACEMENTS[m]
+	}
+	return s
+}
